f8117ec853d94534e608e710921c8f3db66157cb
Dhvani Thakkar <113112823+dhvanithakkar@users.noreply.github.com>
2024-05-23 23:39:46 +0530

    Create Set_default_value_triggers..sql

---
+++ backend/Set_default_value_triggers..sql
@@ -0,0 +1,17 @@
+DELIMITER //
+CREATE TRIGGER set_team_cash_before_insert
+BEFORE INSERT ON Teams
+FOR EACH ROW
+BEGIN
+  SET NEW.CurrentCash = (SELECT InitialCash FROM Competitions WHERE CompetitionID = NEW.CompetitionID);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER set_available_shares_on_insert
+BEFORE INSERT ON Stocks
+FOR EACH ROW
+BEGIN
+  SET NEW.AvailableShares = NEW.TotalShares;
+END //
+DELIMITER ;
